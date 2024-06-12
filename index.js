e0ed44980f16fa471297f554a12aaaddd63579a4
miasumon <miasumon.ru@gmail.com>
2024-06-12 12:47:07 +0600

    patch the agreements api created

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 const express = require('express')
 const app = express()
+
 require('dotenv').config()
+
 const cors = require('cors')
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000
+
+
 
 // 2QeH9UkVq61CnU4A
 // GreenLifeStyle
@@ -36,24 +41,34 @@ async function run() {
 
         const apartmentsCollection = client.db('GreenLifeStyle').collection('apartments')
         const agreementsCollection = client.db('GreenLifeStyle').collection('agreements')
+        const paymentsCollection = client.db('GreenLifeStyle').collection('payments')
 
         // get the all apartments data
 
-        app.get('/apartments', async(req, res)=> {
+        app.get('/apartments', async (req, res) => {
 
-            const result = await apartmentsCollection.find().toArray()
+            const page = parseInt(req.query.page)
+            const size = parseInt(req.query.size)
+
+            const result = await apartmentsCollection
+            .find()
+            .skip(page * size)
+            .limit(size)
+            .toArray()
 
             res.send(result)
 
         })
 
+
+
         // get the one apartment data by id
 
-        app.get('/apartments/:id', async(req, res)=> {
+        app.get('/apartments/:id', async (req, res) => {
 
             const id = req.params.id
 
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
 
             const result = await apartmentsCollection.findOne(query)
 
@@ -63,26 +78,177 @@ async function run() {
 
         // insert the user information with apartment details
 
-        app.post('/agreements', async(req, res)=> {
+        app.post('/agreements/:email', async (req, res) => {
 
             const agreement = req.body
+            const email = req.params.email
 
             // checking the apartment available or not 
 
-            const query = {apartmentNo : agreement.apartmentNo}
+            const query = { userEmail: email }
             const isExist = await agreementsCollection.findOne(query)
 
-            if(isExist){
-                return res.send({message : true})
+            if (isExist) {
+                return res.send({ message: true })
             }
 
             // insert the agreement after checking the availability of the apartment
 
             const result = await agreementsCollection.insertOne(agreement)
-           
+
             res.send(result)
 
         })
+
+
+        // get all agreements
+
+        app.get('/agreementsAll/:email', async(req, res)=> {
+            const email  = req.params.email
+            const query = { userEmail : email}
+
+            const result = await agreementsCollection.findOne(query)
+
+            res.send(result)
+
+
+
+        })
+
+            // checking role
+
+            app.get('/agreements/:email', async(req, res)=> {
+
+                const email = req.params.email
+                console.log(email)
+
+                const query = {userEmail : email}
+
+                const isExist = await agreementsCollection.findOne(query)
+
+                console.log(isExist)
+
+                if(!isExist){
+                    return
+                }
+
+                const role = isExist.role
+
+
+                //    if the role is admin
+
+                if(role === 'admin'){
+
+                    const inquiry = {role :'member'}
+                    const options = {            
+                        // Include only the `title` and `imdb` fields in the returned document
+                        projection: { userEmail: 1, userName: 1},
+                      };
+                  
+                    const members = await agreementsCollection.find(inquiry, options).toArray()
+
+                   return res.send({role, members})
+                }
+
+                res.send({role})
+
+
+            })
+
+            // remove the membership
+
+           app.patch('/agreements/:id', async(req, res)=> {
+
+            const id = req.params.id
+
+            const role = req.body.status
+
+            const query = {_id : new ObjectId(id)}
+
+            const updateDoc = {
+                $set: {
+                  role : role
+                }
+              };
+
+              const result = await agreementsCollection.updateOne(query, updateDoc)
+
+              res.send(result)
+
+
+           })
+
+
+        // pagination related api
+        app.get("/apartmentsCount", async (req, res) => {
+
+            const count = await apartmentsCollection.estimatedDocumentCount()
+            res.send({count} )
+        })
+
+         // payment intent
+
+    app.post('/create-payment-intent', async(req, res) => {
+
+        const {price} = req.body
+        const amount = parseInt(price * 100)
+  
+
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount : amount,
+          currency : 'usd',
+          payment_method_types : ['card']
+        })
+  
+  
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        })
+  
+  
+         
+      })
+
+       // payment save
+
+    app.post('/payments', async(req, res)=> {
+
+        const payment = req.body
+  
+      const paymentResult = await paymentsCollection.insertOne(payment)
+  
+      // carefully delete each item from the cart
+  
+    //   const query = {
+    //     _id : {
+    //       $in : payment.cartId.map(id => new ObjectId(id) )
+    //     }
+    //   }
+  
+    //   const deleteResult = await cartCollection.deleteMany(query)
+  
+      res.send(paymentResult)
+  
+  
+      })
+
+      
+    // get the all payments api
+
+    app.get('/payments/:email', async(req, res)=> {
+
+        const email = req.params.email
+  
+        const query = { email : email}
+  
+        const result = await paymentsCollection.find(query).toArray()
+  
+        res.send(result)
+  
+
+  
+      })
+  
 
 
 
