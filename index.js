c0c7932eefc9423d99ebbe1a426176b3be7e9471
miasumon <miasumon.ru@gmail.com>
2024-06-21 23:38:36 +0600

    added necessary links in the cors

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.use(
     cors({
       origin: [
         "http://localhost:5173",
+        "http://localhost:5174",
         "https://assignment-twelve-client-e914a.web.app",
-        "https://assignment-twelve-client-e914a.firebaseapp.com/",
+        "https://assignment-twelve-client-e914a.firebaseapp.com",
       ]
     })
   );
